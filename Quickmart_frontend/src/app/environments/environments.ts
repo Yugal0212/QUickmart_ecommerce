86f0f05b6523478e9a3563412d30569a4216d4de
jakasaniya yugal <jakasaniyayugal@gmail.com>
2026-06-03 13:37:24 +0530

    Update frontend environment to point to live Render URL

--- Quickmart_frontend/src/app/environments/environments.ts
+++ Quickmart_frontend/src/app/environments/environments.ts
@@ -1,8 +1,7 @@
 export const environment = {
-    production: false,
-    apiUrl : 'http://localhost:5000/api',
-    // apiUrl: 'https://ecommerce-web-gfrz.onrender.com/api',
-    encryptionKey:""
+    production: true,
+    apiUrl: 'https://quickmart-ecommerce.onrender.com/api',
+    encryptionKey: ""
   };
   
   
